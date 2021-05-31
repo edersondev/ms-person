abc11cd8eada32acdc1c3d25d1a062529736d9ec
Ederson Ferreira <ederson.dev@gmail.com>
2021-05-31 09:16:44 -0300

    Project created

---
+++ src/test/java/com/edersonferreira/msperson/MsPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edersonferreira.msperson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
