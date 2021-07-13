4f54fe610a33531e7e04666c4618b2a8298fcd64
Ederson Ferreira <ederson.dev@gmail.com>
2021-07-13 01:12:11 -0300

    Refactoring PersonService

--- src/main/java/com/edersonferreira/msperson/repositories/CountryRepository.java
+++ src/main/java/com/edersonferreira/msperson/repositories/CountryRepository.java
@@ -1,10 +1,12 @@
 package com.edersonferreira.msperson.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.edersonferreira.msperson.model.entities.Country;
 
 public interface CountryRepository extends JpaRepository<Country, Long> {
 
-	Country findByIsoCode3(String isoCode3);
+	Optional<Country> findByIsoCode3(String isoCode3);
 }
--- src/main/java/com/edersonferreira/msperson/repositories/PersonRepository.java
+++ src/main/java/com/edersonferreira/msperson/repositories/PersonRepository.java
@@ -1,5 +1,7 @@
 package com.edersonferreira.msperson.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,5 +13,5 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
 
 	@Query("SELECT new com.edersonferreira.msperson.dto.PersonDTO(obj) "
 			+ "FROM Person AS obj INNER JOIN obj.documents AS doc WHERE doc.documentType = 1 AND doc.number = :nucpf")
-	PersonDTO findByCpf(@Param("nucpf") String nucpf);
+	Optional<PersonDTO> findByCpf(@Param("nucpf") String nucpf);
 }
